959f250be052e598b25637517cc1c47f808158ef
May.Wu <may.wu@datacube.hk>
2023-12-22 15:31:38 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 // 引入需要的模块
 import { createRouter, createWebHashHistory } from "vue-router";
+// import { createRouter, createWebHistory } from "vue-router";
 import Home from '@/views/home.vue';
 
 const routes = [
@@ -15,7 +16,8 @@ const routes = [
 
 // 创建路由
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory(), // 配合electron打包, 要用hash路由
+  // history: createWebHistory(),
   routes
 });
 
